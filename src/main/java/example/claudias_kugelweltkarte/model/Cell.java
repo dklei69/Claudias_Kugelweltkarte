e12feb9856b980e48b75b8cd810ede18b26da706
d.kiel <danielakiel@yahoo.com>
2026-04-13 15:35:08 +0200

    13.04.22026

---
+++ src/main/java/example/claudias_kugelweltkarte/model/Cell.java
@@ -0,0 +1,35 @@
+package example.claudias_kugelweltkarte.model;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Cell {
+    @Setter(AccessLevel.NONE)
+    private BiomeType biomeType;
+    private int x;
+    private int y;
+    private boolean isStabilized;
+    private boolean isFixed;
+
+    //Konstruktor
+    public Cell(int x, int y, BiomeType biomeType, boolean isStabilized) {
+        this.x = x;
+        this.y = y;
+        this.biomeType = biomeType;
+        this.isStabilized = isStabilized;
+    }
+
+    public Cell(int x, int y, BiomeType biomeType) {
+        this(x, y, biomeType, false);
+    }
+
+    public void setBiomeType(BiomeType biomeType) {
+        if (!isStabilized && !isFixed) {
+            this.biomeType = biomeType;
+        }
+    }
+
+}
